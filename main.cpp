6b043b7134767bd0ed15472e54c8cd429d3d0bd8
kozikzbazin <kozikdaking@gmail.com>
2023-12-24 17:16:16 +0100

    player class update

--- main.cpp
+++ main.cpp
@@ -1,24 +1,46 @@
 #include <SFML/Graphics.hpp>
+#include "player.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+   //-------------------------------INITIALIZE----------------------------
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "GAME");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+    window.setFramerateLimit(240);
+   //-------------------------------INITIALIZE----------------------------
+    Player player;
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+   //-------------------------------INITIALIZE----------------------------
+ 
+    player.Initialize();
+   //-------------------------------INITIALIZE----------------------------
+   
+   //--------------------------------LOAD----------------------------
+   player.Load();
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+   //--------------------------------LOAD----------------------------
+  
+   //--------------------------------UPDATE----------------------------
+   while (window.isOpen())
+   {
+       sf::Event event;
+       while (window.pollEvent(event))
+       {
+           if (event.type == sf::Event::Closed)
+               window.close();
+       }
+
+       player.Update();
+       //--------------------------------UPDATE----------------------------
+
+       //--------------------------------DRAW----------------------------
+       window.clear();
+       player.Draw(window);
+       window.display();
+   }
+       //--------------------------------DRAW----------------------------
+    
 
     return 0;
 }
